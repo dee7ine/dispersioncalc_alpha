11.09 - added isotropic calculation class, fixed minor issues

main.py
@@ -3,16 +3,14 @@ import scipy.misc
 from isotropic_main import show_image, test_func
 from Decorators import timeit
 
-# Define the tab content as classes:
 class IsotropicMaterials(wx.Panel):
     def __init__(self, parent):
         wx.Panel.__init__(self, parent)
-        wx.StaticText(self, -1, "hohohooho", (100, 100))
+        wx.StaticText(self, -1, "checkmark", (100, 100))
 
 class AnisotropicMaterials(wx.Panel):
     def __init__(self, parent):
         wx.Panel.__init__(self, parent)
-
 
 class TabThree(wx.Panel):
     def __init__(self, parent):
@@ -42,16 +40,18 @@ class TabEight(wx.Panel):
 class MainFrame(wx.Frame):
     @timeit
     def __init__(self):
-        wx.Frame.__init__(self, None, title="Tree Simulator 9000")
+        wx.Frame.__init__(self, None, title="disp calc")
 
         """
-         Create a panel and notebook (tabs holder)
+        tabs holder
         """
 
         p = wx.Panel(self)
         nb = wx.Notebook(p)
 
-        # Create the tab windows
+        """
+        tabs management
+        """
         tab1 = IsotropicMaterials(nb)
         tab2 = AnisotropicMaterials(nb)
         tab3 = TabThree(nb)
@@ -61,16 +61,17 @@ class MainFrame(wx.Frame):
         tab7 = TabSeven(nb)
         tab8 = TabSeven(nb)
 
-        cb1 = wx.CheckBox(tab1, label='Marcin', pos=(20, 20))
+        cb1 = wx.CheckBox(tab1, label='checkmark', pos=(20, 20))
+        cb1.SetValue(False)
+        cb1 = wx.CheckBox(tab1, label='checkmark', pos=(20, 40))
         cb1.SetValue(False)
 
-        cb2 = wx.CheckBox(tab2, label='TIME', pos=(200, 40))
+        cb2 = wx.CheckBox(tab2, label='checkmark', pos=(200, 40))
         cb2.SetValue(True)
 
-        cb3 = wx.CheckBox(tab3, label='TIME', pos=(128, 128))
+        cb3 = wx.CheckBox(tab3, label='checkmark', pos=(128, 128))
         cb3.SetValue(True)
 
-        # Add the windows to tabs and name them.
         nb.AddPage(tab1, "Isotropic")
         nb.AddPage(tab2, "Anisotropic")
         nb.AddPage(tab3, "Signal simulator")
@@ -80,7 +81,9 @@ class MainFrame(wx.Frame):
         nb.AddPage(tab7, "Material editor")
         nb.AddPage(tab8, "Advanced")
 
-        # Set noteboook in a sizer to create the layout
+        """
+        layout management
+        """
         sizer = wx.BoxSizer()
         sizer.Add(nb, 1, wx.EXPAND)
         p.SetSizer(sizer)
@@ -94,49 +97,27 @@ class MainFrame(wx.Frame):
 
         :return:
         """
-        """
-        A menu bar is composed of menus, which are composed of menu items.
-        This method builds a set of menus and binds handlers to be called
-        when the menu item is selected.
-        """
 
-        # Make a file menu with Hello and Exit items
         fileMenu = wx.Menu()
-        # The "\t..." syntax defines an accelerator key that also triggers
-        # the same event
+
         helloItem = fileMenu.Append(-1, "&Hello...\tCtrl-H",
                 "Help string shown in status bar for this menu item")
         fileMenu.AppendSeparator()
-        # When using a stock ID we don't need to specify the menu item's
-        # label
+
         exitItem = fileMenu.Append(wx.ID_EXIT)
 
-        # Now a help menu for the about item
+
         helpMenu = wx.Menu()
         aboutItem = helpMenu.Append(wx.ID_ABOUT)
 
-        #about menu
         aboutMenu = wx.Menu()
         about1 = aboutMenu.Append(wx.ID_ABOUT)
         about2 = aboutMenu.Append(wx.ID_ABOUT)
         about3  = aboutMenu.Append(wx.ID_ABOUT)
 
-        #materials menu
         materialsMenu = wx.Menu()
-
-        #multicore menu
         multicoreMenu = wx.Menu()
-
-        #homepage menu
         homepageMenu = wx.Menu()
-
-        """
-        # Make the menu bar and add the two menus to it. The '&' defines
-        # that the next letter is the "mnemonic" for the menu item. On the
-        # platforms that support it those letters are underlined and can be
-        # triggered from the keyboard.
-        
-        """
 
         menuBar = wx.MenuBar()
         menuBar.Append(fileMenu, "&File")
@@ -146,7 +127,6 @@ class MainFrame(wx.Frame):
         menuBar.Append(helpMenu, "&Help")
         menuBar.Append(aboutMenu, "&About")
 
-        # Give the menu bar to the frame
         self.SetMenuBar(menuBar)
         """
         associate a handler function with the EVT_MENU event for
@@ -162,10 +142,6 @@ class MainFrame(wx.Frame):
         :param event:
         :return:
         """
-        """
-        
-        Close the frame, terminating the application.
-        """
 
         self.Close(True)
 
@@ -175,22 +151,17 @@ class MainFrame(wx.Frame):
         :param event:
         :return:
         """
-
-        """Say hello to the user."""
-        wx.MessageBox("Gruszczyk alert\n Run")
+        wx.MessageBox("default")
 
     def OnAbout(self, event):
         """Display an About Dialog"""
-        wx.MessageBox("This is a wxPython Hello World sample",
-                      "About Hello World 2",
+        wx.MessageBox("link to be generated",
                       wx.OK|wx.ICON_INFORMATION)
         show_image()
 
 if __name__ == "__main__":
     app = wx.App()
-    #MainFrame().Show() # do wyjebania
     frm = MainFrame()
-    #frm.SetIcon(wx.IconFromLocation("C:/Users/xjlksd/Downloads/tree.ico")) #do wyjebania (?)
     frm.Show()
     frm.SetIcon(wx.Icon(r"C:\Users\deefi\PycharmProjects\dispersioncalc_alpha\cropped3815.ico"))
     app.MainLoop()