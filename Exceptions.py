f32ed0f41548146401df75af3af88856add31f6e
deefi <dee7ine@gmail.com>
2022-12-09 20:04:32 +0100

    11.12 - updated documentation for utilities; added close all figures feature

--- Exceptions.py
+++ Exceptions.py
@@ -1,8 +1,18 @@
 class NoMaterialFound(Exception):
     pass
 
+
 class IncorrectMode(Exception):
     pass
 
+
 class ErrorParsingMaterial(Exception):
+    pass
+
+
+class IncorrectFilePath(Exception):
+    pass
+
+
+class IncorrectFileFormat(Exception):
     pass
